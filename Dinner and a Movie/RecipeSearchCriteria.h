b2ab8124030e1360e12401e68b3ea315f078f149
Joe Blough <jdb@Web-Services-MacBook-Pro.local>
2012-05-03 17:30:48 -0400

    Added searching to recipes

---
+++ Dinner and a Movie/RecipeSearchCriteria.h
@@ -0,0 +1,16 @@
+//
+//  RecipeSearchCriteria.h
+//  Dinner and a Movie
+//
+//  Created by Joe Blough on 5/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RecipeSearchCriteria : NSObject
+
+@property (nonatomic, strong) NSString *nameFilter;
+@property (nonatomic, strong) NSString *ingredientFilter;
+
+@end
---
+++ Dinner and a Movie/RecipeSearchCriteria.m
@@ -0,0 +1,16 @@
+//
+//  RecipeSearchCriteria.m
+//  Dinner and a Movie
+//
+//  Created by Joe Blough on 5/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "RecipeSearchCriteria.h"
+
+@implementation RecipeSearchCriteria
+
+@synthesize nameFilter = _nameFilter;
+@synthesize ingredientFilter = _ingredientFilter;
+
+@end
