2018ca38e30787d390cfdb57adbd372d8fc35635
Joe Blough <jdb@Web-Services-MacBook-Pro.local>
2012-05-22 03:40:33 -0400

    Fix indentation

--- Dinner and a Movie/UIAlertView+Blocks.m
+++ Dinner and a Movie/UIAlertView+Blocks.m
@@ -16,37 +16,35 @@ static CancelBlock _cancelBlock;
                       otherButtonTitles:(NSArray*) otherButtons
                               onDismiss:(DismissBlock) dismissed                   
                                onCancel:(CancelBlock) cancelled {
-  
-  _cancelBlock  = [cancelled copy];
-  
-  _dismissBlock  = [dismissed copy];
-  
-  UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
-                                                  message:message
-                                                 delegate:[self self]
-                                        cancelButtonTitle:cancelButtonTitle
-                                        otherButtonTitles:nil];
-  
-  for(NSString *buttonTitle in otherButtons)
-    [alert addButtonWithTitle:buttonTitle];
-  
-
+    
+    _cancelBlock  = [cancelled copy];
+    _dismissBlock  = [dismissed copy];
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
+                                                    message:message
+                                                   delegate:self
+                                          cancelButtonTitle:cancelButtonTitle
+                                          otherButtonTitles:nil];
+    
+    for(NSString *buttonTitle in otherButtons)
+        [alert addButtonWithTitle:buttonTitle];
+    
     alert.alertViewStyle = UIAlertViewStylePlainTextInput;
     
-  [alert show];
-  return alert;
+    [alert show];
+    return alert;
 }
 
 + (void)alertView:(UIAlertView*) alertView didDismissWithButtonIndex:(NSInteger) buttonIndex {
-  
+    
 	if(buttonIndex == [alertView cancelButtonIndex])
 	{
 		_cancelBlock();
 	}
-  else
-  {
-    _dismissBlock([[alertView textFieldAtIndex:0] text]);
-  }  
+    else
+    {
+        _dismissBlock([[alertView textFieldAtIndex:0] text]);
+    }  
 }
 
 
